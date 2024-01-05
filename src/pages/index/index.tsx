7fbfe29c1d471faf843e228911c7f450d02b64be
lg <galle2002@163.com>
2024-01-06 00:47:56 +0800

    add taro-ui

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,6 +1,8 @@
 import { Component, PropsWithChildren } from 'react'
 import { View, Text } from '@tarojs/components'
+import { AtButton } from 'taro-ui'
 import './index.scss'
+import 'taro-ui/dist/style/index.scss' // 全局引入一次即可
 
 export default class Index extends Component<PropsWithChildren> {
   componentDidMount () { }
@@ -13,11 +15,10 @@ export default class Index extends Component<PropsWithChildren> {
 
   render () {
     return (
-      <View className='index'>
-        <Text>Hello world!</Text>
-        <Text>Ha Ha Ha</Text>
-        <div>xxxx</div>
-        <Text>Hello world!</Text>
+      <View className='page'>
+        <AtButton>按钮文案</AtButton>
+        <AtButton type='primary'>按钮文案</AtButton>
+        <AtButton type='secondary'>按钮文案</AtButton>
       </View>
     )
   }
